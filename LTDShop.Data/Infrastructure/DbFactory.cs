e0b6fc4ca58555fd160ffc4cdc42e3523d34b9a3
your name <03trungduck29@gmail.com>
2023-03-23 17:28:48 +0700

    Thiết kế class cho phần Entity framwork Code First

---
+++ LTDShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTDShop.Data.Infrastructure
+{
+   public class DbFactory : Disposable, IDbFactory
+    {
+         LTDShopDbContext dbContext;
+
+        public LTDShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new LTDShopDbContext());
+        }
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
